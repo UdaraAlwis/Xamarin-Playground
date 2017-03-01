fdb1bbc97b0a6a23829519943ae771754b4906ae
Udara Alwis <udara.robotics@gmail.com>
2017-03-02 00:58:24 +0800

    Added comments

--- XFNavBarBackBtnClickOverride/XFNavBarBackBtnClickOverride/XFNavBarBackBtnClickOverride.iOS/CustomContentPageRenderer.cs
+++ XFNavBarBackBtnClickOverride/XFNavBarBackBtnClickOverride/XFNavBarBackBtnClickOverride.iOS/CustomContentPageRenderer.cs
@@ -27,51 +27,68 @@ namespace XFNavBarBackBtnClickOverride.iOS
 
         private void SetCustomBackButton()
         {
-            var backBtnImage = UIImage.FromBundle("iosbackarrow.png");
+            // Load the Back arrow Image
+			var backBtnImage = UIImage.FromBundle("iosbackarrow.png");
 
-            backBtnImage = backBtnImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
+			backBtnImage = 
+				backBtnImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
 
-            var backBtn = new UIButton(UIButtonType.Custom)
-            {
-                HorizontalAlignment = UIControlContentHorizontalAlignment.Left,
-                TitleEdgeInsets = new UIEdgeInsets(11.5f, 15f, 10f, 0f),
-                ImageEdgeInsets = new UIEdgeInsets(1f, 8f, 0f, 0f)
-            };
+			// Create our Button and set Edge Insets for Title and Image
+			var backBtn = new UIButton(UIButtonType.Custom)
+			{
+				HorizontalAlignment = UIControlContentHorizontalAlignment.Left,
+				TitleEdgeInsets = new UIEdgeInsets(11.5f, 15f, 10f, 0f),
+				ImageEdgeInsets = new UIEdgeInsets(1f, 8f, 0f, 0f)
+			};
 
-            backBtn.SetTitle("Back", UIControlState.Normal);
-            backBtn.SetTitleColor(UIColor.FromRGB(0, 129, 249), UIControlState.Normal);
-            backBtn.SetTitleColor(UIColor.LightGray, UIControlState.Highlighted);
-            backBtn.SetImage(backBtnImage, UIControlState.Normal);
-            backBtn.Font = UIFont.FromName("HelveticaNeue", (nfloat)17);
-            backBtn.SizeToFit();
-            backBtn.TouchDown += (sender, e) =>
-            {
-                if (((CoolContentPage)Element).CustomBackButtonAction != null)
-                {
-                    ((CoolContentPage)Element).CustomBackButtonAction. Invoke();
-                }
-            };
-            
-            //Set the frame of the button
-            backBtn.Frame = new CGRect(
-                0,
-                0,
-                UIScreen.MainScreen.Bounds.Width / 4,
-                NavigationController.NavigationBar.Frame.Height);
+			// Set the styling for Title
+			// You could set any Text as you wish here
+			backBtn.SetTitle("Back", UIControlState.Normal);
+			// use the default blue color in ios back button text
+			backBtn.SetTitleColor(UIColor.FromRGB(0, 129, 249), UIControlState.Normal); 
+			backBtn.SetTitleColor(UIColor.LightGray, UIControlState.Highlighted);
+			backBtn.Font = UIFont.FromName("HelveticaNeue", (nfloat)17);
 
-            var btnContainer = new UIView(new CGRect(0, 0, backBtn.Frame.Width, backBtn.Frame.Height));
-            btnContainer.AddSubview(backBtn);
+			// Set the Image to the button
+			backBtn.SetImage(backBtnImage, UIControlState.Normal);
 
-            var fixedSpace = new UIBarButtonItem(UIBarButtonSystemItem.FixedSpace)
-            {
-                Width = -16f
-            };
-            var backButtonItem = new UIBarButtonItem("", UIBarButtonItemStyle.Plain, null)
-            {
-                CustomView = backBtn
-            };
+			// Allow the button to Size itself
+			backBtn.SizeToFit();
 
-            NavigationController.TopViewController.NavigationItem.LeftBarButtonItems = new[] { fixedSpace, backButtonItem };
-        }
+			// Add the Custom Click event you would like to 
+			// execute upon the Back button click
+			backBtn.TouchDown += (sender, e) =>
+			{
+				// Whatever your custom back button click handling
+			};
+
+			//Set the frame of the button
+			backBtn.Frame = new CGRect(
+				0,
+				0,
+				UIScreen.MainScreen.Bounds.Width / 4,
+				NavigationController.NavigationBar.Frame.Height);
+
+			// Add our button to a container
+			var btnContainer = new UIView(
+				new CGRect(0, 0, backBtn.Frame.Width, backBtn.Frame.Height));
+			btnContainer.AddSubview(backBtn);
+
+			// A dummy button item to push our custom  back button to
+			// the edge of screen (sort of a hack)
+			var fixedSpace = new UIBarButtonItem(UIBarButtonSystemItem.FixedSpace)
+			{
+				Width = -16f
+			};
+			// wrap our custom back button with a UIBarButtonItem
+			var backButtonItem = new UIBarButtonItem("", UIBarButtonItemStyle.Plain, null)
+			{
+				CustomView = backBtn
+			};
+
+			// Add it to the ViewController
+			NavigationController.TopViewController.NavigationItem.LeftBarButtonItems 
+			= new[] { fixedSpace, backButtonItem };
+		}
     }
 }
