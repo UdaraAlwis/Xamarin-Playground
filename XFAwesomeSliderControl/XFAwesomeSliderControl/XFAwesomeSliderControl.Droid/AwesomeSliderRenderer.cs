1180b892f369bbe609a0622cc47def2fce100097
udara.alwis <udara.alwis@tippstr.com>
2017-03-07 19:38:46 +0800

    improve

--- XFAwesomeSliderControl/XFAwesomeSliderControl/XFAwesomeSliderControl.Droid/AwesomeSliderRenderer.cs
+++ XFAwesomeSliderControl/XFAwesomeSliderControl/XFAwesomeSliderControl.Droid/AwesomeSliderRenderer.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Content.Res;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -14,6 +15,8 @@ using Xamarin.Forms.Platform.Android;
 using XFAwesomeSliderControl.Droid;
 using XFAwesomeSliderControl;
 using Android.Graphics;
+using Android.Graphics.Drawables;
+using Color = Android.Graphics.Color;
 
 [assembly:ExportRenderer(typeof(AwesomeSlider), typeof(AwesomeSliderRenderer))]
 namespace XFAwesomeSliderControl.Droid
@@ -26,7 +29,17 @@ namespace XFAwesomeSliderControl.Droid
 
             if (e.NewElement != null)
             {
-                this.Control.Thumb.SetColorFilter(Android.Graphics.Color.Red, PorterDuff.Mode.SrcIn);
+                // Set custom drawable resource
+                Control.SetProgressDrawableTiled(Resources.GetDrawable(Resource.Drawable.custom_progressbar_style, (this.Context).Theme));
+
+                //// Hide thumb
+                //Control.SetThumb(new ColorDrawable(Color.Transparent));
+
+                //// progressbar and progressbar background color
+                //Control.ProgressDrawable.SetColorFilter(
+                //    new PorterDuffColorFilter(
+                //    Xamarin.Forms.Color.FromHex("#ff0066").ToAndroid(),
+                //    PorterDuff.Mode.SrcIn));
             }
         }
     }
