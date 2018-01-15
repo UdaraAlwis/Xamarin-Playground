4c55aced2d52edd4b39c948ebf4bf41c7d561bce
udaraxamariners <udara@xamariners.com>
2018-01-15 23:03:20 +0800

    wootz!

--- XFAwesomeSpinnerView/XFAwesomeSpinnerView/XFAwesomeSpinnerView.iOS/SpinnerService.cs
+++ XFAwesomeSpinnerView/XFAwesomeSpinnerView/XFAwesomeSpinnerView.iOS/SpinnerService.cs
@@ -61,37 +61,13 @@ namespace XFAwesomeSpinnerView.iOS
                 var renderer = xamFormsPage.GetOrCreateRenderer();
 
                 nativeView = renderer.NativeView;
-
-                //splash1.SizeToFit();
-                //splash1.Alpha = 0.7f;
-
+                
                 isInitialized = true;
             }
 
             UIApplication.SharedApplication.KeyWindow.AddSubview(nativeView);
         }
-
-        // Credits: https://michaelridland.com/xamarin/creating-native-view-xamarin-forms-viewpage/
-        public static UIView ConvertFormsToNative(Xamarin.Forms.View view, CGRect size)
-        {
-            var renderer = Platform.CreateRenderer(view);
-
-            //var renderer = RendererFactory.GetRenderer(view);
-
-            renderer.NativeView.Frame = size;
-
-            renderer.NativeView.AutoresizingMask = UIViewAutoresizing.All;
-            renderer.NativeView.ContentMode = UIViewContentMode.ScaleToFill;
-
-            renderer.Element.Layout(size.ToRectangle());
-
-            var nativeView = renderer.NativeView;
-
-            nativeView.SetNeedsLayout();
-
-            return nativeView;
-        }
-
+        
         public void CloseSpinner()
         {
             nativeView.RemoveFromSuperview();
