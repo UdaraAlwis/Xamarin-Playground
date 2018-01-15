82b7476f757ba8a6bb799d9dd9ac2e8bda4b8baa
Udara Alwis <udara.robotics@gmail.com>
2018-01-16 04:35:13 +0800

    finally fixed at dawn

---
+++ XFAnimatingDotsControl/XFAnimatingDotsControl/AnimatingDotsControl.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFAnimatingDotsControl
+{
+    public partial class AnimatingDotsControl : ContentView
+    {
+        public AnimatingDotsControl()
+        {
+            InitializeComponent();
+        }
+
+        private bool animationStarted;
+        
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == "Renderer")
+            {
+                if (!animationStarted)
+                {
+                    // start the animation on element rendering
+                    animationStarted = true;
+
+                    RunAnimations();
+                }
+                else
+                {
+                    // abort the animation on element disposing
+                    this.AbortAnimation("loadingIndicatorPulseAnimation");
+                }
+            }
+        }
+
+        private void RunAnimations()
+        {
+            var pulseAnimation1 = new Animation();
+
+            pulseAnimation1.Add(0, 0.33, new Animation(alpha => Dot1.Opacity = alpha, 1, 0, Easing.CubicOut, () => Dot1.FadeTo(1)));
+            pulseAnimation1.Add(0.33, 0.66, new Animation(alpha => Dot2.Opacity = alpha, 1, 0, Easing.CubicOut, () => Dot2.FadeTo(1)));
+            pulseAnimation1.Add(0.66, 0.99, new Animation(alpha => Dot3.Opacity = alpha, 1, 0, Easing.CubicOut, () => Dot3.FadeTo(1)));
+            
+            pulseAnimation1.Commit(this, "loadingIndicatorPulseAnimation", 10, 1100, null, null, () => true);            
+        }
+    }
+}
