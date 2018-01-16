940c0918e6dc29ba09688ae9fcdc09a5cb83e348
udaraxamariners <udara@xamariners.com>
2018-01-16 21:50:02 +0800

    moving to new project

---
+++ XFLoadingDialogService/XFLoadingDialogService/XFLoadingDialogService/ISpinnerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFLoadingDialogService
+{
+    public interface ISpinnerService
+    {
+        void OpenSpinner();
+
+        void CloseSpinner();
+    }
+}
