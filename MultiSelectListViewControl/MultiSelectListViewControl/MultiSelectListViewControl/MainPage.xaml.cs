62a1426fd376b0073a0156128ed7713a63e24233
Udara Alwis <udara.robotics@gmail.com>
2018-03-22 09:40:18 +0800

    multi select listview

---
+++ MultiSelectListViewControl/MultiSelectListViewControl/MultiSelectListViewControl/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MultiSelectListViewControl
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
+        }
+	}
+}
---
+++ MultiSelectListViewControl/MultiSelectListViewControl/MultiSelectListViewControl/MainPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MultiSelectListViewControl
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<string> Data { get; set; }
+    }
+}
