0b4a6beec3c073e9f773dd77617337c48553318a
udaraxamariners <udara@xamariners.com>
2018-03-29 23:23:43 +0800

    yaay added unittest with xunit and test app mocking in prism

---
+++ AdvPrismTabNavigation/AdvPrismTabNavigation.xUnitTest/FirstTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvPrismTabNavigation.ViewModels;
+using AdvPrismTabNavigation.Views;
+using Prism.Common;
+using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Xamarin.Forms;
+using Xunit;
+
+namespace AdvPrismTabNavigation.xUnitTest
+{
+    public class FirstTest
+    {
+        private TestApp _appInstance;
+
+        public FirstTest()
+        {
+            Xamarin.Forms.Mocks.MockForms.Init();
+        }
+
+        [Fact]
+        public void AppInit()
+        {
+            _appInstance = new TestApp();
+
+            Assert.NotNull(_appInstance);
+        }
+
+        [Fact]
+        public void HomePageInit()
+        {
+            _appInstance = new TestApp();
+
+            //HomePage homePage = new HomePage();
+            //ViewModelLocator.SetAutowireViewModel(homePage, true);
+
+            //Assert.NotNull(homePage.BindingContext);
+            //Assert.IsType<HomePageViewModel>(homePage.BindingContext);
+            
+            // Am I in the HomePage?
+            Assert.IsType<HomePage>(PageUtilities.GetCurrentPage(_appInstance.MainPage));
+
+            // Let's go to Tabbed Page
+            var homePageViewModel = _appInstance.Container.Resolve<HomePageViewModel>();
+            homePageViewModel.GoToTabPageCommand.Execute();
+            _appInstance.Container.Resolve<MyTabbedPage>().SendAppearing();
+
+            //  Am I in the MyTabbedPage-> TabChild1Page?
+            Assert.IsType<TabChild1Page>(PageUtilities.GetCurrentPage(_appInstance.MainPage));
+
+            //  Let's Tab-Navigate to TabChild2Page
+            _appInstance.Container.Resolve<TabChild1PageViewModel>().GoToNextTabCommand.Execute("1");
+
+            //  Am I in the MyTabbedPage-> TabChild2Page?
+            Assert.IsType<TabChild2Page>(_appInstance.Container.Resolve<MyTabbedPage>().CurrentPage);
+
+            //  Let's Tab-Navigate to TabChild3Page
+            _appInstance.Container.Resolve<TabChild2PageViewModel>().GoToNextTabCommand.Execute("2");
+
+            //  Am I in the MyTabbedPage-> TabChild2Page?
+            Assert.IsType<TabChild3Page>(_appInstance.Container.Resolve<MyTabbedPage>().CurrentPage);
+        }
+    }
+}
---
+++ AdvPrismTabNavigation/AdvPrismTabNavigation.xUnitTest/TestApp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvPrismTabNavigation.ViewModels;
+using AdvPrismTabNavigation.Views;
+using Prism;
+using Prism.Common;
+using Prism.Ioc;
+using Prism.Navigation;
+using Prism.Unity;
+using Unity;
+using Xamarin.Forms;
+
+namespace AdvPrismTabNavigation.xUnitTest
+{
+    public partial class TestApp : PrismApplication
+    {
+        /* 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public TestApp() : this(null) { }
+
+        public TestApp(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            Xamarin.Forms.Mocks.MockForms.Init();
+
+            Container.GetContainer().RegisterInstance<INavigationService>(NavigationService);
+
+            await NavigationService.NavigateAsync("NavigationPage/HomePage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<HomePage>();
+            containerRegistry.RegisterForNavigation<MyTabbedPage>();
+            containerRegistry.RegisterForNavigation<TabChild1Page>();
+            containerRegistry.RegisterForNavigation<TabChild2Page>();
+            containerRegistry.RegisterForNavigation<TabChild3Page>();
+            containerRegistry.RegisterForNavigation<DetailPage>();
+        }
+    }
+}
