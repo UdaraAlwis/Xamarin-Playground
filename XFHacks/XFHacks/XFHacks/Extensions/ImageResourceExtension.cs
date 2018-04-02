732da148ee84c2c7fc680b063398162e67fa5ea0
Udara Alwis <udara.robotics@gmail.com>
2018-04-02 09:49:12 +0800

    xfhacks

---
+++ XFHacks/XFHacks/XFHacks/Extensions/ImageResourceExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFHacks.Extensions
+{
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource(Source);
+
+            return imageSource;
+        }
+    }
+}
---
+++ XFHacks/XFHacks/XFHacks/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFHacks
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PickerWithIconPage());
+        }
+    }
+}
