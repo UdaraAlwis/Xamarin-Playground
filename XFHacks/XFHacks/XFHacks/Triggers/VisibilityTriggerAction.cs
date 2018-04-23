a5cdaaa4ba0165e20651c643a41ba39f975fdf25
udaraxamariners <udara@xamariners.com>
2018-04-23 19:59:15 +0800

    kapow

---
+++ XFHacks/XFHacks/XFHacks/Triggers/VisibilityTriggerAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFHacks.Triggers
+{
+    /// <summary>
+    /// A simple trigger to change a View's visibility dynamically
+    /// </summary>
+    public class VisibilityTriggerAction : TriggerAction<View>
+    {
+        public bool IsViewVisible { get; set; }
+
+        protected override void Invoke(View sender)
+        {
+            sender.IsVisible = IsViewVisible;
+        }
+    }
+}
