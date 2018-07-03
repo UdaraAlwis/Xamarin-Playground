198b3310af42fba7c670ad1a318796f8a522416a
Udara Alwis <udara.robotics@gmail.com>
2018-07-04 03:02:11 +0800

    banana minion qhy the society

--- XFSegmentedControl/XFSegmentedControl/XFSegmentedControl/MainPage.xaml.cs
+++ XFSegmentedControl/XFSegmentedControl/XFSegmentedControl/MainPage.xaml.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace XFSegmentedControl
 {
-	public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+
+
 		}
-	}
+
+        private void SegmentedControlView_SelectedTabIndexChanged(object sender, SelectedTabIndexEventArgs e)
+        {
+            if (e.SelectedTabIndex == 1)
+            {
+                ContentView1.IsVisible = true;
+                ContentView2.IsVisible = false;
+            }
+            if (e.SelectedTabIndex == 2)
+            {
+                ContentView1.IsVisible = false;
+                ContentView2.IsVisible = true;
+            }
+        }
+    }
 }
--- XFSegmentedControl/XFSegmentedControl/XFSegmentedControl/SegmentedControl.xaml.cs
+++ XFSegmentedControl/XFSegmentedControl/XFSegmentedControl/SegmentedControl.xaml.cs
@@ -39,7 +39,35 @@ namespace XFSegmentedControl
 	        set { SetValue(SecondaryColorProperty, value); }
 	    }
 
-        
+
+        public static readonly BindableProperty Tab1TextProperty
+            = BindableProperty.Create(
+                nameof(Tab1Text),
+                typeof(string),
+                typeof(SegmentedControl),
+                default(string));
+
+        public string Tab1Text
+        {
+            get { return (string)GetValue(Tab1TextProperty); }
+            set { SetValue(Tab1TextProperty, value); }
+        }
+
+
+        public static readonly BindableProperty Tab2TextProperty
+            = BindableProperty.Create(
+                nameof(Tab2Text),
+                typeof(string),
+                typeof(SegmentedControl),
+                default(string));
+
+        public string Tab2Text
+        {
+            get { return (string)GetValue(Tab2TextProperty); }
+            set { SetValue(Tab2TextProperty, value); }
+        }
+
+
         public static readonly BindableProperty SelectedTabIndexProperty
             = BindableProperty.Create(
                 nameof(SelectedTabIndex),
@@ -50,57 +78,70 @@ namespace XFSegmentedControl
         public int SelectedTabIndex
         {
             get { return (int)GetValue(SelectedTabIndexProperty); }
-            set { SetValue(SelectedTabIndexProperty, value); }
+            private set { SetValue(SelectedTabIndexProperty, value); }
         }
-        
 
-        public SegmentedControl ()
+        public event EventHandler<SelectedTabIndexEventArgs> SelectedTabIndexChanged;
+
+        public SegmentedControl()
 		{
-			InitializeComponent ();
+			InitializeComponent();
+        }
 
-		    if (Device.RuntimePlatform == Device.Android)
-		    {
-		        Tab1LabelView.FontSize = Device.GetNamedSize(NamedSize.Medium, Tab1LabelView);
-		        Tab2LabelView.FontSize = Device.GetNamedSize(NamedSize.Medium, Tab1LabelView);
+        protected override void OnParentSet()
+        {
+            base.OnParentSet();
+            
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                Tab1LabelView.FontSize
+                    = Device.GetNamedSize(NamedSize.Medium, Tab1LabelView);
+                Tab2LabelView.FontSize
+                    = Device.GetNamedSize(NamedSize.Medium, Tab1LabelView);
 
-		        Tab1ButtonView.BackgroundColor = PrimaryColor;
-		        Tab2ButtonView.BackgroundColor = PrimaryColor;
+                Tab1ButtonView.BackgroundColor = PrimaryColor;
+                Tab2ButtonView.BackgroundColor = PrimaryColor;
 
-                Tab1BoxView.Color = SecondaryColor;
-		        Tab2BoxView.Color = SecondaryColor;
-
-		        Tab1LabelView.TextColor = SecondaryColor;
-		        Tab2LabelView.TextColor = SecondaryColor;
+                Tab1BoxView.Color =
+                Tab2BoxView.Color =
+                Tab1LabelView.TextColor =
+                Tab2LabelView.TextColor = SecondaryColor;
             }
-		    else if (Device.RuntimePlatform == Device.iOS)
-		    {
-		        Tab1LabelView.FontSize = Device.GetNamedSize(NamedSize.Small, Tab1LabelView);
-		        Tab2LabelView.FontSize = Device.GetNamedSize(NamedSize.Small, Tab1LabelView);
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                Tab1LabelView.FontSize
+                    = Device.GetNamedSize(NamedSize.Small, Tab1LabelView);
+                Tab2LabelView.FontSize
+                    = Device.GetNamedSize(NamedSize.Small, Tab1LabelView);
 
-		        Tab1ButtonView.BackgroundColor = PrimaryColor;
-		        Tab2ButtonView.BackgroundColor = PrimaryColor;
+                Tab1ButtonView.BackgroundColor =
+                Tab2ButtonView.BackgroundColor = PrimaryColor;
 
-		        FrameView.BorderColor = SecondaryColor;
-
-		        Tab1LabelView.TextColor = SecondaryColor;
-		        Tab2LabelView.TextColor = SecondaryColor;
+                FrameView.BorderColor =
+                Tab1LabelView.TextColor =
+                Tab2LabelView.TextColor = SecondaryColor;
             }
 
-		    SelectTab1();
+            Tab1LabelView.Text = Tab1Text;
+            Tab2LabelView.Text = Tab2Text;
+
+            SelectTab1();
         }
 
         private void Tab1Button_OnClicked(object sender, EventArgs e)
 	    {
             SelectTab1();
 	        SelectedTabIndex = 1;
-	    }
+            SendSelectedTabIndexChangedEvent();
+        }
 
         private void Tab2Button_OnClicked(object sender, EventArgs e)
         {
             SelectTab2();
             SelectedTabIndex = 2;
+            SendSelectedTabIndexChangedEvent();
         }
-
+        
         private void SelectTab1()
         {
             if (Device.RuntimePlatform == Device.Android)
@@ -134,5 +175,18 @@ namespace XFSegmentedControl
                 Tab2LabelView.TextColor = PrimaryColor;
             }
         }
+
+        private void SendSelectedTabIndexChangedEvent()
+        {
+            var eventArgs = new SelectedTabIndexEventArgs();
+            eventArgs.SelectedTabIndex = SelectedTabIndex;
+
+            SelectedTabIndexChanged?.Invoke(this, eventArgs);
+        }
+    }
+
+    public class SelectedTabIndexEventArgs : EventArgs
+    {
+        public int SelectedTabIndex { get; set; }
     }
 }
