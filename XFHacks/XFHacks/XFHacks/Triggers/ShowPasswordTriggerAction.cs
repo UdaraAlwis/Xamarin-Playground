79e44273bdd4cd85b46a780f86be39a8fa2e3bdd
Udara Alwis <udara.robotics@gmail.com>
2018-07-19 19:17:15 +0800

    comments

--- XFHacks/XFHacks/XFHacks/Triggers/ShowPasswordTriggerAction.cs
+++ XFHacks/XFHacks/XFHacks/Triggers/ShowPasswordTriggerAction.cs
@@ -3,6 +3,10 @@ using Xamarin.Forms;
 
 namespace XFHacks.Triggers
 {
+    /// <summary>
+    /// The Trigger Action that will handle
+    /// the Show/Hide Passeword text
+    /// </summary>
     public class ShowPasswordTriggerAction : TriggerAction<Button>
     {
         public string IconImageName { get; set; }
@@ -13,24 +17,38 @@ namespace XFHacks.Triggers
 
         protected override void Invoke(Button sender)
         {
-            var imageIconView = ((Grid) sender.Parent).FindByName<Image>(IconImageName);
-            var entryPasswordView = ((Grid) ((Grid) sender.Parent).Parent).FindByName<Entry>(EntryPasswordName);
-            var entryTextView = ((Grid)((Grid)sender.Parent).Parent).FindByName<Entry>(EntryTextName);
+            // get the runtime references for our Elements from our custom control
+            var imageIconView = ((Grid) sender.Parent)
+                                    .FindByName<Image>(IconImageName);
+            var entryPasswordView = ((Grid) ((Grid) sender.Parent).Parent)
+                                    .FindByName<Entry>(EntryPasswordName);
+            var entryTextView = ((Grid)((Grid)sender.Parent).Parent)
+                                    .FindByName<Entry>(EntryTextName);
 
+            // Switch visibility of Password Entry field and Text Entry fields
             entryPasswordView.IsVisible = !entryPasswordView.IsVisible;
             entryTextView.IsVisible = !entryTextView.IsVisible;
 
+            // update the Show/Hide button Icon states 
             if (entryPasswordView.IsVisible)
             {
-                imageIconView.Source = ImageSource.FromResource("XFHacks.Resources.showpasswordicon.png",
+                // Password is not Visible state
+                imageIconView.Source = ImageSource.FromResource(
+                    "XFHacks.Resources.showpasswordicon.png",
                     Assembly.GetExecutingAssembly());
+
+                // Setting up Entry curser focus
                 entryPasswordView.Focus();
                 entryPasswordView.Text = entryTextView.Text;
             }
             else
             {
-                imageIconView.Source = ImageSource.FromResource("XFHacks.Resources.hidepasswordicon.png",
+                // Password is Visible state
+                imageIconView.Source = ImageSource.FromResource(
+                    "XFHacks.Resources.hidepasswordicon.png",
                     Assembly.GetExecutingAssembly());
+
+                // Setting up Entry curser focus
                 entryTextView.Focus();
                 entryTextView.Text = entryPasswordView.Text;
             }
