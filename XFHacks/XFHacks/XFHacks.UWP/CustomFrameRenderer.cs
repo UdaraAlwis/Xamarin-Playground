48a9e1a1880f8310734a3fd1c936f67c3f8b917f
Udara Alwis <udara.robotics@gmail.com>
2018-08-13 01:50:36 +0800

    blingo

---
+++ XFHacks/XFHacks/XFHacks.UWP/CustomFrameRenderer.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms.Platform.UWP;
+using Xamarin.Forms;
+using XFHacks.UWP;
+
+[assembly: ExportRenderer(typeof(Frame), typeof(CustomFrameRenderer))]
+namespace XFHacks.UWP
+{
+    class CustomFrameRenderer : FrameRenderer
+    {
+        private Color backgroundColor;
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                backgroundColor = e.NewElement.BackgroundColor;
+                UpdateBackgroundColor();
+            }
+        }
+
+        protected override void UpdateBackgroundColor()
+        {
+            //base.UpdateBackgroundColor();
+            if (Control != null)
+            {
+                var nativeControl = (Windows.UI.Xaml.Controls.Border)Control;
+                var nativeColor = Windows.UI.Color.FromArgb(
+                                                                (byte)(backgroundColor.A * 255),
+                                                                (byte)(backgroundColor.R * 255),
+                                                                (byte)(backgroundColor.G * 255),
+                                                                (byte)(backgroundColor.B * 255));
+                nativeControl.Background = new Windows.UI.Xaml.Media.SolidColorBrush(nativeColor);
+            }
+        }
+
+    }
+}
