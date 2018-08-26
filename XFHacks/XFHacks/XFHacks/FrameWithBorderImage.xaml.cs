612666085da87e861da5c5838fac31b4e8e2271d
Udara Alwis <udara.robotics@gmail.com>
2018-08-26 20:45:53 +0800

    change oftitle

--- XFHacks/XFHacks/XFHacks/FrameWithBorderImage.xaml.cs
+++ XFHacks/XFHacks/XFHacks/FrameWithBorderImage.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace XFHacks
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class LabelWithBorderPage : ContentPage
+	public partial class FrameWithBorderImage : ContentPage
 	{
-		public LabelWithBorderPage()
+		public FrameWithBorderImage ()
 		{
 			InitializeComponent ();
 		}
