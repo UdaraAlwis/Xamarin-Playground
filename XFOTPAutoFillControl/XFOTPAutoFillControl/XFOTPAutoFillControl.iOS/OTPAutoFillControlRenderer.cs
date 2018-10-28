abd26c200ce2287b74c137987f16c4fc32a66e41
Udara Alwis <udara.robotics@gmail.com>
2018-10-28 12:47:12 +0800

    otp project

---
+++ XFOTPAutoFillControl/XFOTPAutoFillControl/XFOTPAutoFillControl.iOS/OTPAutoFillControlRenderer.cs
@@ -0,0 +1,22 @@
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XFOTPAutoFillControl;
+using XFOTPAutoFillControl.iOS;
+
+[assembly: ExportRenderer(typeof(OTPAutoFillControl), typeof(OTPAutoFillControlRenderer))]
+namespace XFOTPAutoFillControl.iOS
+{
+    public class OTPAutoFillControlRenderer: EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                Control.TextContentType = UITextContentType.OneTimeCode;
+            }
+        }
+    }
+}
---
+++ XFOTPAutoFillControl/XFOTPAutoFillControl/XFOTPAutoFillControl/OTPAutoFillControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFOTPAutoFillControl
+{
+    /// <summary>
+    /// OTP AutoFill Control for Xamarin.Forms
+    /// </summary>
+    public class OTPAutoFillControl : Entry
+    {
+
+    }
+}
