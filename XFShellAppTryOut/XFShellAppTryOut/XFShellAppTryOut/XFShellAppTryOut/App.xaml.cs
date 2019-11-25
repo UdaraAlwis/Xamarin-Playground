dbf4867be79dd48b0f5e78b5d079d94b4acab780
Udara Alwis <udara.robotics@gmail.com>
2019-11-25 15:44:16 +0800

    shell app

---
+++ XFShellAppTryOut/XFShellAppTryOut/XFShellAppTryOut/XFShellAppTryOut/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XFShellAppTryOut.Services;
+using XFShellAppTryOut.Views;
+
+namespace XFShellAppTryOut
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XFShellAppTryOut/XFShellAppTryOut/XFShellAppTryOut/XFShellAppTryOut/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XFShellAppTryOut.Models;
+
+namespace XFShellAppTryOut.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
