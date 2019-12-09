e3b3dbab493c1df74aa96562c3b1d38a09c7c16b
Udara Alwis <udara.robotics@gmail.com>
2019-12-09 14:39:36 +0800

    pingu

--- XFInteropTest/XFInteropTest/XFInteropTest.Android/MainApplication.cs
+++ XFInteropTest/XFInteropTest/XFInteropTest.Android/MainApplication.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
-using Android.OS;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Plugin.CurrentActivity;
 
 namespace XFInteropTest.Droid
