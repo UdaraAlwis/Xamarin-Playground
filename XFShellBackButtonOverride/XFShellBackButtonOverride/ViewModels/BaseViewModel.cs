3d6aaa202a9b8d08177258516f9a369120f19e8d
Joe Sauve <joe@joesauve.com>
2020-12-30 16:01:26 -0600

    Demonstrating how you don't need to manually implement INotifyPropertyChanged. Use the PropertyChanged.Fody nuget to make your life easier.

--- XFShellBackButtonOverride/XFShellBackButtonOverride/ViewModels/BaseViewModel.cs
+++ XFShellBackButtonOverride/XFShellBackButtonOverride/ViewModels/BaseViewModel.cs
@@ -1,49 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using PropertyChanged;
 
 namespace XFShellBackButtonOverride.ViewModels
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    [AddINotifyPropertyChangedInterface]
+    public abstract class BaseViewModel
     {
-        bool isBusy = false;
-        public bool IsBusy
-        {
-            get { return isBusy; }
-            set { SetProperty(ref isBusy, value); }
-        }
+        /* 
+         * With PropertyChanged.Fody, You can throw out all the excessive boilerplate.
+         * 
+         * Just decorate your base viewmodel with [AddINotifyPropertyChangedInterface].
+         * 
+         * It achieves the exact same effect as manually imoplementing INotifyPropertyChanged.
+         * 
+         * By annotating with [AddINotifyPropertyChangedInterface], all your public getters automatically 
+         * have code injected at compile time that implments property-change notifications.
+         * 
+         * I also frequently annoate my models, even my EF Core models (because EF Core will just ignore
+         * the attribute, while my app code will honor it). That way all your model properties also automatically
+         * emit property-changed notifications.
+         * 
+         * 
+         * All your base viewmodel needs is this:
+         */
 
-        string title = string.Empty;
-        public string Title
-        {
-            get { return title; }
-            set { SetProperty(ref title, value); }
-        }
+        public bool IsBusy { get; protected set; } // You can use the the regular public get/set if you want to. I'm assuming that IsBusy should never be set outside of a viewmodel class.
 
-        protected bool SetProperty<T>(ref T backingStore, T value,
-            [CallerMemberName] string propertyName = "",
-            Action onChanged = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(backingStore, value))
-                return false;
+        public string Title { get; protected set; } // You can use the the regular public get/set if you want to. I'm assuming that Title should never be set outside of a viewmodel class.
 
-            backingStore = value;
-            onChanged?.Invoke();
-            OnPropertyChanged(propertyName);
-            return true;
-        }
+        /* You can throw all of this away:
+         * 
+       bool isBusy = false;
+       public bool IsBusy
+       {
+           get { return isBusy; }
+           set { SetProperty(ref isBusy, value); }
+       }
 
-        #region INotifyPropertyChanged
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            var changed = PropertyChanged;
-            if (changed == null)
-                return;
+       string title = string.Empty;
+       public string Title
+       {
+           get { return title; }
+           set { SetProperty(ref title, value); }
+       }
 
-            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        #endregion
+       protected bool SetProperty<T>(ref T backingStore, T value,
+           [CallerMemberName] string propertyName = "",
+           Action onChanged = null)
+       {
+           if (EqualityComparer<T>.Default.Equals(backingStore, value))
+               return false;
+
+           backingStore = value;
+           onChanged?.Invoke();
+           OnPropertyChanged(propertyName);
+           return true;
+       }
+
+       #region INotifyPropertyChanged
+       public event PropertyChangedEventHandler PropertyChanged;
+       protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+       {
+           var changed = PropertyChanged;
+           if (changed == null)
+               return;
+
+           changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+       }
+       #endregion
+
+       */
     }
 }
