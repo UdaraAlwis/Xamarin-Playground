aa0d13ec3eb6041b1142f91824bb983cbb6f9876
Udara Alwis <udara.robotics@gmail.com>
2021-02-14 22:38:12 +0800

    refined

--- XFFillInAnswersLayout/XFFillInAnswersLayout/MainPage.xaml.cs
+++ XFFillInAnswersLayout/XFFillInAnswersLayout/MainPage.xaml.cs
@@ -21,8 +21,7 @@ namespace XFFillInAnswersLayout
             base.OnAppearing();
 
             string fillInTheBlanksQuestion =
-                "THERE IS A {MONKEY} EATING {PIZZA} WITH {BANANA SLICES} DRESSED UP IN A RED {JUMP SUITE}! BUT HIS " +
-                "FRIEND WANTS TO WEAR A WHAT? A FLYING {SPACE SUITE} WITH AN ACTUAL HELMET ON {IT'S HEAD!} I MEAN, IS HE FOR REAL!?";
+                "THERE IS A {MONKEY} EATING {PIZZA} WITH {BANANA SLICES} DRESSED UP IN A RED {JUMP SUITE}!";
 
             // Extract the marked blanks fields from the input text
             var matches = Regex.Matches(fillInTheBlanksQuestion, "\\{(.*?)\\}", RegexOptions.IgnoreCase);
@@ -32,38 +31,37 @@ namespace XFFillInAnswersLayout
             {
                 answerIndexList.Add(index, item.Value); ;
                 var regex = new Regex(Regex.Escape(item.Value));
+                // Mark out the answer blanks with a unique signature ex: {[0]}, {[1]}
                 fillInTheBlanksQuestion = regex.Replace(fillInTheBlanksQuestion, "{[" + index++ + "]}", 1);
             }
 
-            // Create the words array from the 
+            // Segment out the words from the input text
             string[] wordsArray = fillInTheBlanksQuestion.Split(' ');
 
             fillInTheBlanksQuestionPanel.Children.Clear();
+            // Populate the segmented words in the Layout
             for (int i = 0; i < wordsArray.Length; i++)
             {
                 string word = wordsArray[i];
                 if (Regex.IsMatch(word, "(\\{\\[[0-9]\\]\\})"))
                 {
+                    // This is an answer input segment
                     var answerFieldIndex = int.Parse(Regex.Match(word, "[0-9]", RegexOptions.IgnoreCase).Value);
                     var placeholderForAnswerField = answerIndexList[answerFieldIndex] as string;
 
+                    // Create the Entry field
                     fillInTheBlanksQuestionPanel.Children.Add(new Entry()
                     {
                         Placeholder = placeholderForAnswerField.Replace("{", "").Replace("}", ""),
                         FontSize = 20,
                         HeightRequest = 45,
-                    });
-                    fillInTheBlanksQuestionPanel.Children.Add(new Label()
-                    {
-                        Text = " ", 
-                        BackgroundColor = Color.SkyBlue,
-                        VerticalTextAlignment = TextAlignment.Center,
-                        FontSize = 20,
-                        HeightRequest = 45,
+                        Margin = 2,
+                        BackgroundColor = Color.BurlyWood,
                     });
                 }
                 else
                 {
+                    // This is a normal text segment
                     fillInTheBlanksQuestionPanel.Children.Add(new Label()
                     {
                         Text = word + " ",
@@ -71,6 +69,7 @@ namespace XFFillInAnswersLayout
                         VerticalTextAlignment = TextAlignment.Center,
                         FontSize = 20,
                         HeightRequest = 45,
+                        Margin = 2,
                     });
                 }
             }
